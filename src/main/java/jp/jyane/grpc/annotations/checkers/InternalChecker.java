c5304cbf8cb04f2b900b54112fae907107a1d773
jyane <jyane.kstm@gmail.com>
2018-01-22 19:30:12 +0900

    implements a bug checker of experimental api annotation

--- src/main/java/jp/jyane/grpc/annotations/checkers/InternalChecker.java
+++ src/main/java/jp/jyane/grpc/annotations/checkers/InternalChecker.java
@@ -16,23 +16,8 @@
 
 package jp.jyane.grpc.annotations.checkers;
 
-import static com.google.errorprone.matchers.Description.NO_MATCH;
-
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.SeverityLevel;
-import com.google.errorprone.VisitorState;
-import com.google.errorprone.bugpatterns.BugChecker;
-import com.google.errorprone.bugpatterns.BugChecker.CompilationUnitTreeMatcher;
-import com.google.errorprone.bugpatterns.BugChecker.IdentifierTreeMatcher;
-import com.google.errorprone.bugpatterns.BugChecker.MemberSelectTreeMatcher;
-import com.google.errorprone.matchers.Description;
-import com.google.errorprone.util.ASTHelpers;
-import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.tree.IdentifierTree;
-import com.sun.source.tree.MemberSelectTree;
-import com.sun.source.tree.Tree;
-import com.sun.tools.javac.code.Symbol;
-import javax.lang.model.element.AnnotationMirror;
 
 @BugPattern(
     name = "Internal",
@@ -40,64 +25,9 @@ import javax.lang.model.element.AnnotationMirror;
     explanation = "@Internal should not be used in application code",
     severity = SeverityLevel.WARNING
 )
-public final class InternalChecker extends BugChecker implements
-    CompilationUnitTreeMatcher, IdentifierTreeMatcher, MemberSelectTreeMatcher {
+public final class InternalChecker extends AnnotationChecker {
 
-  private boolean shouldBeChecked = false;
-
-  /**
-   * Returns true if api is annotated.
-   */
-  private boolean isAnnotatedApi(Symbol symbol) {
-    if (symbol == null) {
-      return false;
-    }
-    for (AnnotationMirror annotation : symbol.getAnnotationMirrors()) {
-      if (annotation.getAnnotationType().toString().equals("io.grpc.Internal")) {
-        return true;
-      }
-    }
-    // recursive
-    return isAnnotatedApi(symbol.owner);
-  }
-
-  /**
-   * Returns the description if tree is annotated.
-   */
-  private Description match(Tree tree, VisitorState state) {
-    Symbol symbol = ASTHelpers.getSymbol(tree);
-    if (!shouldBeChecked || symbol == null) {
-      return NO_MATCH;
-    }
-    if (!isAnnotatedApi(symbol)) {
-      return NO_MATCH;
-    }
-    return describeMatch(tree);
-  }
-
-  @Override
-  public Description matchIdentifier(IdentifierTree tree, VisitorState state) {
-    return match(tree, state);
-  }
-
-  @Override
-  public Description matchMemberSelect(MemberSelectTree tree, VisitorState state) {
-    return match(tree, state);
-  }
-
-  // TODO(jyane): verify order.
-  // Compilation unit tree would be matched first and then other trees would be matched. Really?
-  @Override
-  public Description matchCompilationUnit(CompilationUnitTree tree, VisitorState state) {
-    Tree packageName = tree.getPackageName();
-    if (packageName == null) {
-      return NO_MATCH;
-    }
-    String name = packageName.toString();
-    // The package name is not starting with `io.grpc` or not `io.grpc`.
-    if (!(name.equals("io.grpc") || name.startsWith("io.grpc."))) {
-      shouldBeChecked = true;
-    }
-    return NO_MATCH;
+  public InternalChecker() {
+    super("io.grpc", "io.grpc.Internal");
   }
 }
