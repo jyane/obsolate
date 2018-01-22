7b899c4fa8483582b9adb5aca2dd72e286ec34c6
Shohei Kamimori <jyane.kstm@gmail.com>
2018-01-22 19:39:17 +0900

    implements a bug checker of experimental api annotation (#3)

--- src/main/java/jp/jyane/grpc/annotations/checkers/ExperimentalApiChecker.java
+++ src/main/java/jp/jyane/grpc/annotations/checkers/ExperimentalApiChecker.java
@@ -18,11 +18,6 @@ package jp.jyane.grpc.annotations.checkers;
 
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.BugPattern.SeverityLevel;
-import com.google.errorprone.VisitorState;
-import com.google.errorprone.bugpatterns.BugChecker;
-import com.google.errorprone.bugpatterns.BugChecker.IdentifierTreeMatcher;
-import com.google.errorprone.matchers.Description;
-import com.sun.source.tree.IdentifierTree;
 
 @BugPattern(
     name = "ExperimentalApi",
@@ -30,10 +25,9 @@ import com.sun.source.tree.IdentifierTree;
     explanation = "@ExperimentalApi should not be used in application code",
     severity = SeverityLevel.WARNING
 )
-public class ExperimentalApiChecker extends BugChecker implements IdentifierTreeMatcher {
+public final class ExperimentalApiChecker extends AnnotationChecker {
 
-  @Override
-  public Description matchIdentifier(IdentifierTree identifierTree, VisitorState visitorState) {
-    return null;
+  public ExperimentalApiChecker() {
+    super("io.grpc", "io.grpc.ExperimentalApi");
   }
 }
