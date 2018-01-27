08b2bfbb7c6a25baa79e3c61b57d7debc9d2ec39
Shohei Kamimori <jyane.kstm@gmail.com>
2018-01-28 00:37:06 +0900

    add an example of gradle (#5)

---
+++ examples/gradle/src/main/java/com/example/grpc/Main.java
@@ -0,0 +1,11 @@
+package com.example.grpc;
+
+import io.grpc.InternalStatus;
+import io.grpc.Grpc;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(InternalStatus.MESSAGE_KEY);
+    System.out.println(Grpc.TRANSPORT_ATTR_REMOTE_ADDR);
+  }
+}
